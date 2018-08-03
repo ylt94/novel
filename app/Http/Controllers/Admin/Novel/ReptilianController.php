7dea90b8f9cbb53caf107eb30e20a27807cb0ea0
ylt94 <857425891@qq.com>
2018-08-03 20:27:48 +0800

    update

--- app/Http/Controllers/Admin/Novel/ReptilianController.php
+++ app/Http/Controllers/Admin/Novel/ReptilianController.php
@@ -29,11 +29,12 @@ class ReptilianController extends Controller{
     }
 
     public function getQiDianNovelDetail(){
-        $base_url = 'https://book.qidian.com/info/';
+        $csrf_token = QiDianService::getCsrfToken();
         $novel_id = 1005064061;
-        $url = $base_url.$novel_id;
-        // //$contents = file_get_contents($url);
-
+        //https://book.qidian.com/ajax/book/category?_csrfToken=YcqOui8MNn3esPNxbh4wjlXTEGmz7rpNWKpkjQ1i&bookId=1005064061
+        $url = 'https://book.qidian.com/ajax/book/category?'.http_build_query(['_csrfToken'=>$csrf_token,'bookId'=>$novel_id]);
+        $contents = file_get_contents($url);
+        dd(json_decode($contents,true));
         // // if(!$contents){
         // //     return ['status'=>-1,'msg'=>'查无此书'];
         // // }
@@ -44,32 +45,7 @@ class ReptilianController extends Controller{
         // $url = 'https://www.qidian.com/ajax/Free/getSysTime?_csrfToken=';
         // $contents = file_get_contents($url);dd($contents);
         // $chapters = QiDianService::getNovelChapters($novel_id,$contents);
-        $url = 'https://www.qidian.com/';
-        $oCurl = curl_init();
-        // 设置请求头, 有时候需要,有时候不用,看请求网址是否有对应的要求
-        $header[] = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
-        $user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36";
-        curl_setopt($oCurl, CURLOPT_URL, $url);
-        curl_setopt($oCurl, CURLOPT_HTTPHEADER,$header);
-        // 返回 response_header, 该选项非常重要,如果不为 true, 只会获得响应的正文
-        curl_setopt($oCurl, CURLOPT_HEADER, true);
-        // 是否不需要响应的正文,为了节省带宽及时间,在只需要响应头的情况下可以不要正文
-        curl_setopt($oCurl, CURLOPT_NOBODY, true);
-        // 使用上面定义的 ua
-        curl_setopt($oCurl, CURLOPT_USERAGENT,$user_agent);
-        curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1 );
-
-        // 不用 POST 方式请求, 意思就是通过 GET 请求
-        curl_setopt($oCurl, CURLOPT_POST, false);
-
-        $sContent = curl_exec($oCurl);
-        // 获得响应结果里的：头大小
-        $headerSize = curl_getinfo($oCurl, CURLINFO_HEADER_SIZE);
-        // 根据头大小去获取头信息内容
-        $header = substr($sContent, 0, $headerSize);
-        
-        curl_close($oCurl);  
-        return $headerSize; 
+       
 
     }
     
--- app/Service/Reptilian/QiDianService.php
+++ app/Service/Reptilian/QiDianService.php
@@ -1,5 +1,6 @@
 <?php
     namespace App\Service\Reptilian;
+    use Illuminate\Support\Facades\Cache;
 
 
     class QiDianService {
@@ -65,5 +66,50 @@
             //preg_match_all('/<li data-rid=\"[1-9][0-9]*\">(.*?)<\/li>/',$content, $res);
             dd($res);
             return $chapters;
+        }
+
+        public static function getCsrfToken(){
+            $csrf_token = '';
+            $csrf_token_key = 'qidian_csrf_token';
+            $csrf_token = Cache::get($csrf_token_key);
+            if($csrf_token){
+                return $csrf_token;
+            }
+
+
+            $url = 'https://www.qidian.com/ajax/Help/getCode?_csrfToken=';
+            $oCurl = curl_init();
+            // 设置请求头, 有时候需要,有时候不用,看请求网址是否有对应的要求
+            $header[] = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+            $user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36";
+            curl_setopt($oCurl, CURLOPT_URL, $url);
+            //curl_setopt($oCurl, CURLOPT_HTTPHEADER,$header);
+            // 返回 response_header, 该选项非常重要,如果不为 true, 只会获得响应的正文
+            curl_setopt($oCurl, CURLOPT_HEADER, true);
+            // 是否不需要响应的正文,为了节省带宽及时间,在只需要响应头的情况下可以不要正文
+            curl_setopt($oCurl, CURLOPT_NOBODY, true);
+            // 使用上面定义的 ua
+            //curl_setopt($oCurl, CURLOPT_USERAGENT,$user_agent);
+            curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1 );
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, false);    // 是否需要进行服务端的SSL证书验证
+    
+            // 不用 POST 方式请求, 意思就是通过 GET 请求
+            curl_setopt($oCurl, CURLOPT_POST, false);
+    
+            $sContent = curl_exec($oCurl);
+            // 获得响应结果里的：头大小
+            $headerSize = curl_getinfo($oCurl, CURLINFO_HEADER_SIZE);
+            // 根据头大小去获取头信息内容
+            $header = substr($sContent, 0, $headerSize);
+            
+            curl_close($oCurl);  
+            //_csrfToken=zNBHov91ewRBUkSf2DsjdezDOsSdNWNC3lkBbdKO; expires
+            preg_match_all('/_csrfToken=(.*?);/',$header, $res);
+            if(!$res[1]){
+                return false;
+            }
+            $csrf_token = $res[1][0];
+            Cache::put($csrf_token_key,$csrf_token,90);
+            return $csrf_token;
         }
     }
