d14e7d9cd137f81fef122ce830b2a2fc766d5571
ylt94 <857425891@qq.com>
2018-09-27 23:18:46 +0800

    update

--- resources/views/admin/src/router/index.js
+++ resources/views/admin/src/router/index.js
@@ -1,11 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import index from '@/components/index'
-import Login from '@/components/Login'
-import novels  from '@/components/Novels'
-import novelCategory from '@/components/novelCategory'
-import sites from '@/components/Sites'
-import members from '@/components/members'
 
 Vue.use(Router)
 
@@ -14,48 +8,92 @@ export default new Router({
     {
       path: '/',
       name: '小说管理',
-      component: index,
+      component: resolve => require(['@/components/index'],resolve),
       icon: 'el-icon-document',
       children: [
-        { path: '/novel/category', component: novelCategory,icon: 'el-icon-share', name: '小说类型',},
-        { path: '/novel/novles', component: novels,icon: 'el-icon-share', name: '小说管理',},
-        { path: '/novel/novles-add', component: novelCategory,icon: 'el-icon-share', name: '新增小说',},
+        { 
+          path: '/novel/category', 
+          component: resolve => require(['@/components/novelCategory'],resolve),
+          icon: 'el-icon-share', 
+          name: '小说类型',
+        },
+        { 
+          path: '/novel/novles', 
+          component: resolve => require(['@/components/novels'],resolve),
+          icon: 'el-icon-share', 
+          name: '小说管理',
+        },
+        { 
+          path: '/novel/novles-add', 
+          component: resolve => require(['@/components/novelCategory'],resolve),
+          icon: 'el-icon-share', 
+          name: '新增小说',},
       ]
     },
     {
       path: '/member',
       name: '会员管理',
-      component: index,
+      component: resolve => require(['@/components/index'],resolve),
       icon: 'el-icon-success',
       children: [
-        { path: '/member/members', component: members,icon: 'el-icon-share', name: '会员管理',},
+        { 
+          path: '/member/members', 
+          component: resolve => require(['@/components/members'],resolve),
+          icon: 'el-icon-share', 
+          name: '会员管理',
+        },
       ]
     },
     {
       path: '/statistics',
       name: '统计管理',
-      component: index,
+      component: resolve => require(['@/components/index'],resolve),
       icon: 'el-icon-edit-outline',
       children: [
-        { path: '/statistics/member', component: novelCategory,icon: 'el-icon-share', name: '会员统计',},
-        { path: '/statistics/novel', component: novelCategory,icon: 'el-icon-share', name: '小说统计',},
-        { path: '/statistics/pv', component: novelCategory,icon: 'el-icon-share', name: '访问统计',},
+        { 
+          path: '/statistics/member', 
+          component: resolve =>require(['@/components/novelCategory'],resolve),
+          icon: 'el-icon-share', 
+          name: '会员统计',
+        },
+        { 
+          path: '/statistics/novel', 
+          component: resolve =>require(['@/components/novelCategory'],resolve),
+          icon: 'el-icon-share', 
+          name: '小说统计',
+        },
+        { 
+          path: '/statistics/pv', 
+          component: resolve =>require(['@/components/novelCategory'],resolve),
+          icon: 'el-icon-share', 
+          name: '访问统计',
+        },
       ]
     },
     {
       path: '/sites',
       name: '设置管理',
-      component: index,
+      component: resolve => require(['@/components/index'],resolve),
       icon: 'el-icon-setting',
       children: [
-        { path: '/sites/index', component: sites,icon: 'el-icon-setting', name: '站点设置',},
-        { path: '/process/index', component: sites,icon: 'el-icon-setting', name: '进程设置',},
+        { 
+          path: '/sites/index', 
+          component: resolve =>require(['@/components/sites'],resolve),
+          icon: 'el-icon-setting', 
+          name: '站点设置',
+        },
+        { 
+          path: '/process/index', 
+          component: resolve =>require(['@/components/sites'],resolve),
+          icon: 'el-icon-setting', 
+          name: '进程设置',
+        },
       ]
     },
     {
       path: '/login',
       name: 'login',
-      component: Login,
+      component: resolve => require(['@/components/login'],resolve),
       hidden: true
     },
   ]
