50164234bf40f1abf0574692bb227540b856c0e3
ylt94 <857425891@qq.com>
2018-10-13 23:24:49 +0800

    update

--- app/Console/Commands/NovelBase.php
+++ app/Console/Commands/NovelBase.php
@@ -59,17 +59,16 @@ class NovelBase extends Command
         //     sleep($this->sleep_seconds);
         // }
         $ppid = posix_getpid();
-        dd($ppid);
         $pid = pcntl_fork();
         if ($pid == -1) {
             throw new Exception('fork子进程失败!');
         } elseif ($pid > 0) {
             cli_set_process_title("我是父进程,我的进程id是{$ppid}.");
-            sleep(30);
+            sleep(5);
         } else {
             $cpid = posix_getpid();
             cli_set_process_title("我是{$ppid}的子进程,我的进程id是{$cpid}.");
-            sleep(30);
+            sleep(5);
         }
     }
 }
