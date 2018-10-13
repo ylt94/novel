1e76fe0fcf56ff850e2b5442d6779ac079a3af39
ylt94 <857425891@qq.com>
2018-10-13 23:40:25 +0800

    update

--- app/Console/Commands/NovelBase.php
+++ app/Console/Commands/NovelBase.php
@@ -44,32 +44,79 @@ class NovelBase extends Command
     public function handle()
     {
         //
-        $ppid = posix_getpid();
+        // while(true){
+        //     $time = time()-$this->update_seconds;
+        //     $time = date('Y-m-d H:i:s',$time);
+        //     $novels = NovelBaseModel::where('last_update','<=',$time)->get();
+        //     if(!$novels){
+        //         sleep($this->sleep_seconds);
+        //         continue;
+        //     }
+        //     foreach($novels as $item){
+        //         $this->info($item->id);
+        //         RedisService::setNovelId($item->id);
+        //     }
+        //     sleep($this->sleep_seconds);
+        // }
+        // $ppid = posix_getpid();
+        // $pid = pcntl_fork();
+        // if ($pid == -1) {
+        //     $this->error('fork子进程失败!');
+        // } elseif ($pid > 0) {
+        //     $this->info("我是父进程,我的进程id是{$ppid}.");
+        //     sleep(5);
+        // } else {
+        //     $cpid = posix_getpid();
+        //     $this->info("我是{$ppid}的子进程,我的进程id是{$cpid}.");
+        //     sleep(5);
+        // }
+
         $pid = pcntl_fork();
-        if ($pid == -1) {
-            $this->error('fork子进程失败!');
-        } elseif ($pid > 0) {
-            $this->info("我是父进程,我的进程id是{$ppid}.");
+        
+        if ($pid == -1)
+        {
+            $this->error('fork子进程失败');
+        }
+        elseif ($pid > 0)
+        {
+            //父进程退出,子进程不是进程组长，以便接下来顺利创建新会话
+            exit(0);
+        }
+        
+        // 最重要的一步，创建一个新的会话，脱离原来的控制终端
+        posix_setsid();
+        
+        // 修改当前进程的工作目录，由于子进程会继承父进程的工作目录，修改工作目录以释放对父进程工作目录的占用。
+        chdir('/');
+        
+        /*
+        * 通过上一步，我们创建了一个新的会话组长，进程组长，且脱离了终端，但是会话组长可以申请重新打开一个终端，为了避免
+        * 这种情况，我们再次创建一个子进程，并退出当前进程，这样运行的进程就不再是会话组长。
+        */
+        $pid = pcntl_fork();
+        if ($pid == -1)
+        {
+            throw new Exception('fork子进程失败');
+        }
+        elseif ($pid > 0)
+        {
+            //  再一次退出父进程，子进程成为最终的守护进程
+            exit(0);
+        }
+        
+        // 由于守护进程用不到标准输入输出，关闭标准输入，输出，错误输出描述符
+        fclose(STDIN);
+        fclose(STDOUT);
+        fclose(STDERR);
+        
+        /*
+        * 处理业务代码
+        */
+        
+        while(TRUE)
+        {
+            $this->info('守护进程?');
             sleep(5);
-        } else {
-            $cpid = posix_getpid();
-            $this->info("我是{$ppid}的子进程,我的进程id是{$cpid}.");
-            while(true){
-                // $time = time()-$this->update_seconds;
-                // $time = date('Y-m-d H:i:s',$time);
-                // $novels = NovelBaseModel::where('last_update','<=',$time)->get();
-                // if(!$novels){
-                //     sleep($this->sleep_seconds);
-                //     continue;
-                // }
-                // foreach($novels as $item){
-                //     $this->info($item->id);
-                //     RedisService::setNovelId($item->id);
-                // }
-                // sleep($this->sleep_seconds);
-                $this->info('守护进程了吗？');
-                sleep(5);
-            }
         }
     }
 }
