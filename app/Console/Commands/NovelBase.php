f0f9e3d381efa135ee253e684ae01c81991a6d52
ylt94 <857425891@qq.com>
2018-10-13 22:33:51 +0800

    update

--- app/Console/Commands/NovelBase.php
+++ app/Console/Commands/NovelBase.php
@@ -44,19 +44,31 @@ class NovelBase extends Command
     public function handle()
     {
         //
-        while(true){
-            $time = time()-$this->update_seconds;
-            $time = date('Y-m-d H:i:s',$time);
-            $novels = NovelBaseModel::where('last_update','<=',$time)->get();
-            if(!$novels){
-                sleep($this->sleep_seconds);
-                continue;
-            }
-            foreach($novels as $item){
-                $this->info($item->id);
-                RedisService::setNovelId($item->id);
-            }
-            sleep($this->sleep_seconds);
+        // while(true){
+        //     $time = time()-$this->update_seconds;
+        //     $time = date('Y-m-d H:i:s',$time);
+        //     $novels = NovelBaseModel::where('last_update','<=',$time)->get();
+        //     if(!$novels){
+        //         sleep($this->sleep_seconds);
+        //         continue;
+        //     }
+        //     foreach($novels as $item){
+        //         $this->info($item->id);
+        //         RedisService::setNovelId($item->id);
+        //     }
+        //     sleep($this->sleep_seconds);
+        // }
+        $ppid = posix_getpid();
+        $pid = pcntl_fork();
+        if ($pid == -1) {
+            throw new Exception('fork子进程失败!');
+        } elseif ($pid > 0) {
+            cli_set_process_title("我是父进程,我的进程id是{$ppid}.");
+            sleep(30);
+        } else {
+            $cpid = posix_getpid();
+            cli_set_process_title("我是{$ppid}的子进程,我的进程id是{$cpid}.");
+            sleep(30);
         }
     }
 }
