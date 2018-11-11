731c43af8f121e3d7bb5d8a33a3ffb00b2d5fc20
ylt94 <857425891@qq.com>
2018-11-11 18:23:09 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/', 'Common\IndexController@index');
+Route::get('/novel/{novel_id}','Common\IndexController@novelDetail');
 Route::get('/chapters/{novel_id}','Common\IndexController@novelChapters');
 Route::get('/content/{chapter_id}','Common\IndexController@novelContent');
 Route::get('/content/next/{chapter_id}','Common\IndexController@nextContent');
