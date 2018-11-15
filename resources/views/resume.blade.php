5f53017dca6826512cfae206bc514cca364e8202
ylt94 <857425891@qq.com>
2018-11-15 18:17:22 +0800

    update

--- resources/views/resume.blade.php
+++ resources/views/resume.blade.php
@@ -23,7 +23,7 @@
             color: #636b6f;
             font-family: "microsoft yahei";
             font-weight: 100;
-            height: 100%;
+            min-height: 100%;
             margin: 0;
             background-color:#00c091;
             display:flex;
@@ -37,9 +37,10 @@
         body{
             margin-top:30px;
             background-color:white;
-            height:100%;
             width: 820px;
-            min-height: 1160px;
+            min-height: 100%;
+            border:3px solid #e5e5e5;
+            box-shadow: 0 0 8px 0 rgba(0,0,0,0.2);
         }
         .head{
             margin-top:30px;
@@ -96,6 +97,7 @@
             align-items:center;
             font-weight:800;
             font-size:18px;
+            flex-direction:row;
         }
         .master-item-content{
             color:black;
@@ -113,6 +115,64 @@
             min-width:25%;
             justify-content:center;
         }
+        .work-content{
+            width:100%;
+        }
+        .work-title{
+            height:45px;
+            color:black;
+            width:100%;
+            line-height:45px;
+            font-weight:800;
+            font-size:18px;
+        }
+        .work-item{
+            margin-bottom:30px;
+        }
+        .work-item-title{
+            color:black;
+            height:60px;
+            width:100%;
+            display:flex;
+            font-size:15px;
+            align-items:center;
+            flex-direction:row;
+        }
+        .work-title-item{
+            display:flex;
+            flex:1;
+            min-width:25%;
+            justify-content:flex-start;
+        }
+        .work-item-content{
+            color:black;
+            font-weight:400;
+            font-size:15px;
+            line-height:2;
+            text-indent:35px;
+        }
+        .skill{
+            display:flex;
+            flex-direction:column;
+        }
+        .skill-title{
+            height:45px;
+            color:black;
+            width:100%;
+            line-height:45px;
+            font-weight:800;
+            font-size:18px;
+        }
+        .skill-content{
+            width:100%;
+            display:flex;
+            flex-direction:column;
+        }
+        .skill-item{
+            display:flex;
+            flex-direction:column;
+        }
+        
     </style>
 </head>
 <body>
@@ -148,7 +208,45 @@
                 <div class="master-item-content-item">时间：2013-2017</div>
                 <div class="master-item-content-item">学校：重庆师范大学</div>
                 <div class="master-item-content-item">专业：软件工程(服务外包)</div>
-                <!-- <div class="master-item-content-item">意向岗位：PHP工程师</div> -->
+                <div class="master-item-content-item">学历：本科</div>
+            </div>
+        </div>
+        <div class="work-content">
+            <div class="work-title">工作经历</div>
+            <div class="work-item">
+                <div class="work-item-title">
+                    <div class="work-title-item">时间：2016-8 至 2017-2</div>
+                    <div class="work-title-item">公司：重庆木叶科技有限公司</div>
+                    <div class="work-title-item">职位：PHP工程师</div>
+                </div>
+                <div class="work-item-content">外包公司，负责APP接口开发，网站后端模块开发，如：某教育APP接口开发，某微信公众号转盘抽奖后端开发，某电商网站后端开发（http://www.zgnz168.com）</div>
+            </div>
+
+            <div class="work-item">
+                <div class="work-item-title">
+                    <div class="work-title-item">时间：2017-2 至 2018-5</div>
+                    <div class="work-title-item">公司：重庆翼路科技有限公司</div>
+                    <div class="work-title-item">职位：PHP工程师</div>
+                </div>
+                <div class="work-item-content">公司做自己的产品（零售，进销存）同时也接外包，主要后端框架：laravel，主要前端框架：angularjs，部分项目使用vue，负责后端接口，及接口对应的前端开发，期间因公司需求学会了小程序，主要工作有：进销存模块开发，新零售（分布式）微信三方授权及小程序开发以及外包项目：某法律咨询平台的律师端（小程序）和后台管理系统（laravel+angular）的开发</div>
+            </div>
+
+            <div class="work-item">
+                <div class="work-item-title">
+                    <div class="work-title-item">时间：2018-5 至 今</div>
+                    <div class="work-title-item">公司：重庆嗨客网络科技有限公司</div>
+                    <div class="work-title-item">职位：PHP工程师</div>
+                </div>
+                <div class="work-item-content">支付集成系统（laravel+vue），主要负责支付模块的后端开发维护迭代和硬件（pos机），pc插件接口开发，现进行分布式重写（Yii2+vue）</div>
+            </div>
+            
+        </div>
+        <div class="skill" style="margin-bottom:30px;">
+            <div class="skill-title">技术掌握</div>
+            <div class="skill-content">
+                <div class="skill-item">
+                   1.
+                </div>
             </div>
         </div>
     </div>
