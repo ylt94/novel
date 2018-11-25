58c7e43e104da5d994f4b460d0abd72dd0e909f2
ylt94 <857425891@qq.com>
2018-11-25 18:20:29 +0800

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,6 +20,9 @@ class Kernel extends ConsoleKernel
         \App\Console\Commands\NovelNew::class,
         \App\Console\Commands\NovelContentOne::class,
         \App\Console\Commands\NovelContentTwo::class,
+        \App\Console\Commands\NovelContentThree::class,
+        \App\Console\Commands\NovelContentFour::class,
+        \App\Console\Commands\NovelContentFive::class,
     ];
 
     /**
