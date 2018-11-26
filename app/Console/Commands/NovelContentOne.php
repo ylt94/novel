20e49bae1538bcce9642c6ed8d3d919ca3854570
ylt94 <857425891@qq.com>
2018-11-26 23:17:43 +0800

    update

--- app/Console/Commands/NovelContentOne.php
+++ app/Console/Commands/NovelContentOne.php
@@ -83,6 +83,7 @@ class NovelContentOne extends Command
                 DB::disconnect();
                 sleep(1);
             }catch(\Exception $e){
+                RedisService::setNovelDetailId($detail_id);
                 DB::disconnect();
                 $message = '更新出错：'.$e->getFile().$e->getLine().':'.$e->getMessage();
                 PS::myLog($message,'logs/daemons/novel_content/','error');
