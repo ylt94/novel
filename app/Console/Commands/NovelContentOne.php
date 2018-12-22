481f05622c0051e31c22d3f72f2bdc9a07447fb5
ylt94 <857425891@qq.com>
2018-12-22 21:12:38 +0800

    updagte

--- app/Console/Commands/NovelContentOne.php
+++ app/Console/Commands/NovelContentOne.php
@@ -81,7 +81,7 @@ class NovelContentOne extends Command
                 self::checkChannel($detail_id);
 
                 DB::disconnect();
-                sleep(250);
+                sleep(1);
             }catch(\Exception $e){
                 RedisService::setNovelDetailId($detail_id);
                 DB::disconnect();
