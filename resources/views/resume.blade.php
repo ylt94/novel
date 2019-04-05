0ecbba0a074b58550e4d1bd55fb29bba9c8c6804
ylt94 <857425891@qq.com>
2019-04-05 15:00:58 +0800

    1

--- resources/views/resume.blade.php
+++ resources/views/resume.blade.php
@@ -38,10 +38,11 @@
             margin-top:30px;
             background-color:white;
             margin-bottom:30px;
-            width: 820px;
+            width: 100%;
+            max-width: 820px;
             min-height: 100%;
-            border:3px solid #e5e5e5;
-            box-shadow: 0 0 8px 0 rgba(0,0,0,0.2);
+            /* border:3px solid #e5e5e5;
+            box-shadow: 0 0 8px 0 rgba(0,0,0,0.2); */
         }
         .head{
             margin-top:30px;
@@ -66,22 +67,30 @@
             background-color:auto;
             color:white;
             flex-direction:column;
-            margin-left:30px;
         }
         .name{
             color:auto;
             font-size:30px;
             font-weight:800;
+            margin-left:30px;
         }
-        .desc{
+        .motto{
             padding-left:40px;
             font-size:15px;
             margin-bottom:10px;
+            text-align:right;       
+        }
+        .desc{
+            text-align:right;
+            line-height:40px;
+            font-size:17px;
+            height:60%;
+            width:100%;
         }
         .master{
             margin-top:15px;
             background-color:#f4f4f4;
-            padding: 0 50px 0 50px;
+            padding: 0 5% 0 5%;
         }
         .master-item{
             width:100%;
@@ -99,6 +108,7 @@
             font-weight:800;
             font-size:18px;
             flex-direction:row;
+            text-align:right;
         }
         .master-item-content{
             color:black;
@@ -133,6 +143,8 @@
         .work-item-title{
             color:black;
             height:60px;
+            max-height:60px;
+            /* overflow:hidden; */
             width:100%;
             display:flex;
             font-size:15px;
@@ -143,7 +155,8 @@
             display:flex;
             flex:1;
             min-width:25%;
-            justify-content:flex-start;
+            justify-content:center;
+            line-height:2;
         }
         .work-item-content{
             color:black;
@@ -165,7 +178,10 @@
             font-size:18px;
         }
         .skill-content{
-            width:100%;
+            width:95%;
+            text-indent: 35px;
+            margin-left:5%;
+            line-height: 2;
             display:flex;
             flex-direction:column;
         }
@@ -183,18 +199,16 @@
             margin-top:30px;
             font-size:13px;
             margin-bottom:30px;
-            height:30px;
-            line-height:30px;
+            line-height:2;
         }
         
     </style>
 </head>
 <body>
     <div class="head">
-        <div class="image"></div>
+        <!-- <div class="image"></div> -->
         <div class="base-info">
             <div class="name">袁粒桃</div>
-            <div class="desc"><i>-----生于忧患，死于安乐</i></div>
             <div class="desc">
                 25岁
                 <span style="font-weight:600"> | </span>
@@ -204,6 +218,7 @@
                 <span style="font-weight:600"> | </span>
                 857425891@qq.com
             </div>
+            <div class="motto"><i style="">-----生于忧患，死于安乐</i></div>
         </div>
     </div>
     <div class="master">
@@ -230,7 +245,7 @@
             <div class="work-item">
                 <div class="work-item-title">
                     <div class="work-title-item">时间：2016-8 至 2017-2</div>
-                    <div class="work-title-item">公司：重庆木叶科技有限公司</div>
+                    <div class="work-title-item">公司：重庆木叶科技</div>
                     <div class="work-title-item">职位：PHP工程师</div>
                 </div>
                 <div class="work-item-content">外包公司，负责APP接口开发，网站后端模块开发，如：某教育APP接口开发，某微信公众号转盘抽奖后端开发，某电商网站后端开发（http://www.zgnz168.com）</div>
@@ -239,7 +254,7 @@
             <div class="work-item">
                 <div class="work-item-title">
                     <div class="work-title-item">时间：2017-2 至 2018-5</div>
-                    <div class="work-title-item">公司：重庆翼路科技有限公司</div>
+                    <div class="work-title-item">公司：重庆翼路科技</div>
                     <div class="work-title-item">职位：PHP工程师</div>
                 </div>
                 <div class="work-item-content">公司做自己的产品（零售，进销存）同时也接外包，主要后端框架：laravel，主要前端框架：angularjs，部分项目使用vue，负责后端接口，及接口对应的前端开发，期间因公司需求学会了小程序，主要工作有：进销存模块开发，新零售（分布式）微信三方授权及小程序开发以及外包项目：某法律咨询平台的律师端（小程序）和后台管理系统（laravel+angular）的开发</div>
@@ -248,7 +263,7 @@
             <div class="work-item">
                 <div class="work-item-title">
                     <div class="work-title-item">时间：2018-5 至 今</div>
-                    <div class="work-title-item">公司：重庆嗨客网络科技有限公司</div>
+                    <div class="work-title-item">公司：重庆嗨客网络</div>
                     <div class="work-title-item">职位：PHP工程师</div>
                 </div>
                 <div class="work-item-content">支付集成系统（laravel+vue），主要负责支付模块的后端开发维护迭代和硬件（pos机），pc插件接口开发，现进行分布式重写（Yii2+vue）</div>
@@ -259,24 +274,28 @@
             <div class="skill-title">技术掌握</div>
             <div class="skill-content">
                 <div class="skill-item">
-                   1.能够搭建（docker）LNMP环境,掌握的Linux命令能够满足日常开发所需
+                   1.能够搭建LNMP环境,掌握的Linux命令能够满足日常开发所需,有独立建站经验
                 </div>
                 <div class="skill-item">
                    2.掌握js,jquery,css，能够进行一般页面的编写，掌握vuejs,angularjs,能够搭建初始化环境以及模块编写
                 </div>
                 <div class="skill-item">
-                   3.熟悉redis缓存，熟练使用mysql，并了解SQL优化方案
+                   3.熟悉redis缓存，熟练使用mysql，了解mysql结构及运行原理，掌握sql优化，锁，事务相关知识
                 </div>
                 
                 <div class="skill-item">
-                   4.熟悉微信小程序开发
+                   4.会开发微信小程序
                 </div>
                 <div class="skill-item">
-                   5.熟悉PHP，对于代码质量比较偏执，不断追求优雅，低冗余，高可用的编写境界
+                   5.熟悉PHP，了解基本运行原理，熟悉使用laravel,Yii2，掌握原生swoole
                 </div>
             </div>
+            <div class="skill-title">感兴趣的语言</div>
+            <div class="skill-content">
+                golang、dart
+            </div>
         </div>
-        <div class="remark"><i>注：谢绝毫无意义的外包、长期性经常性的加班、有大牛带最好</i></div>
+        <div class="remark"><i>注：谢绝毫无意义的外包、长期性经常性的加班(不想进icu)、有大牛带最好</i></div>
     </div>
 </body>
 </html>
