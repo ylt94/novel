cdcddec237d5b95cd61d492c6d41a735a3b7d897
ylt94 <857425891@qq.com>
2020-02-23 17:00:49 +0800

    robot conf

--- app/Http/Controllers/Client/ClientController.php
+++ app/Http/Controllers/Client/ClientController.php
@@ -15,8 +15,6 @@ class ClientController extends Controller {
 
 
     public function index(Request $request){
-        usleep(100);
-        return 'success';
         $novel_type = $request->novel_type ?: 0;
         $order_type = $request->order_type ?: 'recommend';
         $return = [];
